fdeed905df6fdccf0f479978933252f7fb4a540c
Aakash K <aakasharyan47@gmail.com>
2022-07-29 15:02:35 +0000

    axi_defines.v

--- level3_design/axi_defines.v
+++ level3_design/axi_defines.v
@@ -1,68 +1,180 @@
-`ifndef __AXI_DEFINES__
-`define __AXI_DEFINES__
+`timescale 1ps / 1ps
 
-//AXI Defines
+module axi_lite_demo #(
+  parameter ADDR_WIDTH          = 32,
+  parameter DATA_WIDTH          = 32,
+  parameter STROBE_WIDTH        = (DATA_WIDTH / 8)
+)(
+  input                               clk,
+  input                               rst,
 
-//--Burst
-`define AXI_BURST_FIXED         2'b00
-`define AXI_BURST_INCR          2'b01
-`define AXI_BURST_WRAP          2'b10
+  //Write Address Channel
+  input                               i_awvalid,
+  input       [ADDR_WIDTH - 1: 0]     i_awaddr,
+  output                              o_awready,
 
-//--Burst Size
-`define AXI_BURST_SIZE_8BIT     3'b000
-`define AXI_BURST_SIZE_16BIT    3'b001
-`define AXI_BURST_SIZE_32BIT    3'b010
-`define AXI_BURST_SIZE_64BIT    3'b011
-`define AXI_BURST_SIZE_128BIT   3'b100
-`define AXI_BURST_SIZE_256BIT   3'b101
-`define AXI_BURST_SIZE_512BIT   3'b110
-`define AXI_BURST_SIZE_1024BIT  3'b111
+  //Write Data Channel
+  input                               i_wvalid,
+  output                              o_wready,
+  input       [STROBE_WIDTH - 1:0]    i_wstrb,
+  input       [DATA_WIDTH - 1: 0]     i_wdata,
 
-//--Response
-`define AXI_RESP_OKAY           2'b00
-`define AXI_RESP_EXOKAY         2'b01
-`define AXI_RESP_SLVERR         2'b10
-`define AXI_RESP_DECERR         2'b11
+  //Write Response Channel
+  output                              o_bvalid,
+  input                               i_bready,
+  output      [1:0]                   o_bresp,
 
-//--Lock
-`define AXI_LOCK_NORMAL         2'b00
-`define AXI_LOCK_EXCLUSIVE      2'b01
-`define AXI_LOCK_LOCKED         2'b10
+  //Read Address Channel
+  input                               i_arvalid,
+  output                              o_arready,
+  input       [ADDR_WIDTH - 1: 0]     i_araddr,
 
-//--cache
-//----Bufferable
-`define AXI_CACHE_NON_BUF       1'b0
-`define AXI_CACHE_BUF           1'b1
-//----Cachable
-`define AXI_CACHE_NON_CACHE     1'b0
-`define AXI_CACHE_CACHE         1'b1
-//----Read Allocate
-`define AXI_CACHE_NON_RA        1'b0
-`define AXI_CACHE_RA            1'b1
-//----Write Allocate
-`define AXI_CACHE_NON_WA        1'b0
-`define AXI_CACHE_WA            1'b1
-//----Unused
-`define AXI_CACHE_UNUSED        4'b0000
+  //Read Data Channel
+  output                              o_rvalid,
+  input                               i_rready,
+  output      [1:0]                   o_rresp,
+  output      [DATA_WIDTH - 1: 0]     o_rdata
+);
+//local parameters
 
-//--Protection
-//----ARPROT[0]
-`define AXI_PROT_NORMAL         1'b0
-`define AXI_PROT_PRIVLEDGE      1'b1
+//Address Map
+localparam  ADDR_0      = 0;
+localparam  ADDR_1      = 4;
 
-//----ARPROT[1]
-`define AXI_PROT_SECURE         1'b0
-`define AXI_PROT_NON_SECURE     1'b1
+localparam  MAX_ADDR = ADDR_1;
 
-//----ARPROT[2]
-`define AXI_PROT_DATA           1'b0
-`define AXI_PROT_INST           1'b1
+//registers/wires
 
-//----Unused:
-`define AXI_PROT_UNUSED         {`AXI_PROT_NORMAL, `AXI_PROT_NON_SECURE, `AXI_PROT_DATA}
+//User Interface
+wire [ADDR_WIDTH - 1: 0]    w_reg_address;
+reg                         r_reg_invalid_addr;
 
-//--Low Power Mode
-`define AXI_POWER_LOW           1'b0
-`define AXI_POWER_NORMAL        1'b1
+wire                        w_reg_in_rdy;
+reg                         r_reg_in_ack_stb;
+wire [DATA_WIDTH - 1: 0]    w_reg_in_data;
 
-`endif //__AXI_DEFINES__
+wire                        w_reg_out_req;
+reg                         r_reg_out_rdy_stb;
+reg [DATA_WIDTH - 1: 0]     r_reg_out_data;
+
+
+//TEMP DATA, JUST FOR THE DEMO
+reg [DATA_WIDTH - 1: 0]     r_temp_0;
+reg [DATA_WIDTH - 1: 0]     r_temp_1;
+
+//submodules
+
+//Convert AXI Slave bus to a simple register/address strobe
+axi_lite_slave #(
+  .ADDR_WIDTH         (ADDR_WIDTH           ),
+  .DATA_WIDTH         (DATA_WIDTH           )
+
+) axi_lite_reg_interface (
+  .clk                (clk                  ),
+  .rst                (rst                  ),
+
+
+  .i_awvalid          (i_awvalid            ),
+  .i_awaddr           (i_awaddr             ),
+  .o_awready          (o_awready            ),
+
+  .i_wvalid           (i_wvalid             ),
+  .o_wready           (o_wready             ),
+  .i_wstrb            (i_wstrb              ),
+  .i_wdata            (i_wdata              ),
+
+  .o_bvalid           (o_bvalid             ),
+  .i_bready           (i_bready             ),
+  .o_bresp            (o_bresp              ),
+
+  .i_arvalid          (i_arvalid            ),
+  .o_arready          (o_arready            ),
+  .i_araddr           (i_araddr             ),
+
+  .o_rvalid           (o_rvalid             ),
+  .i_rready           (i_rready             ),
+  .o_rresp            (o_rresp              ),
+  .o_rdata            (o_rdata              ),
+
+
+  //Simple Register Interface
+  .o_reg_address      (w_reg_address        ),
+  .i_reg_invalid_addr (r_reg_invalid_addr   ),
+
+  //Ingress Path (From Master)
+  .o_reg_in_rdy       (w_reg_in_rdy         ),
+  .i_reg_in_ack_stb   (r_reg_in_ack_stb     ),
+  .o_reg_in_data      (w_reg_in_data        ),
+
+  //Egress Path (To Master)
+  .o_reg_out_req      (w_reg_out_req        ),
+  .i_reg_out_rdy_stb  (r_reg_out_rdy_stb    ),
+  .i_reg_out_data     (r_reg_out_data       )
+);
+
+//asynchronous logic
+
+//synchronous logic
+always @ (posedge clk) begin
+  //De-assert Strobes
+  r_reg_in_ack_stb                        <=  0;
+  r_reg_out_rdy_stb                       <=  0;
+  r_reg_invalid_addr                      <=  0;
+
+  if (rst) begin
+    r_reg_out_data                        <=  0;
+
+    //Reset the temporary Data
+    r_temp_0                              <=  0;
+    r_temp_1                              <=  0;
+  end
+  else begin
+
+    if (w_reg_in_rdy) begin
+      //From master
+      case (w_reg_address)
+        ADDR_0: begin
+          //$display("Incomming data on address: 0x%h: 0x%h", w_reg_address, w_reg_in_data);
+          r_temp_0                        <=  w_reg_in_data;
+        end
+        ADDR_1: begin
+          //$display("Incomming data on address: 0x%h: 0x%h", w_reg_address, w_reg_in_data);
+          r_temp_1                        <=  w_reg_in_data;
+        end
+        default: begin
+          $display ("Unknown address: 0x%h", w_reg_address);
+        end
+      endcase
+      if (w_reg_address > MAX_ADDR) begin
+        //Tell the host they wrote to an invalid address
+        r_reg_invalid_addr                <= 1;
+      end
+      //Tell the AXI Slave Control we're done with the data
+      r_reg_in_ack_stb                    <= 1;
+    end
+    else if (w_reg_out_req) begin
+      //To master
+      //$display("User is reading from address 0x%0h", w_reg_address);
+      case (w_reg_address)
+        ADDR_0: begin
+          r_reg_out_data                  <= r_temp_0;
+        end
+        ADDR_1: begin
+          r_reg_out_data                  <= r_temp_1;
+        end
+        default: begin
+          //Unknown address
+          r_reg_out_data                  <= 32'h00;
+        end
+      endcase
+      if (w_reg_address > MAX_ADDR) begin
+        //Tell the host they are reading from an invalid address
+        r_reg_invalid_addr                <= 1;
+      end
+      //Tell the AXI Slave to send back this packet
+      r_reg_out_rdy_stb                   <= 1;
+    end
+  end
+end
+
+endmodule
