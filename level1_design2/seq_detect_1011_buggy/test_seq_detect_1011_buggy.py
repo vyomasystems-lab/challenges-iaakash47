89350968302dab2df3a476a0c0eabb64fa7d04b7
Aakash K <aakasharyan47@gmail.com>
2022-07-30 18:54:46 +0000

    test_seq_detect_1011_fix.py

--- level1_design2/seq_detect_1011_buggy/test_seq_detect_1011_buggy.py
+++ level1_design2/seq_detect_1011_buggy/test_seq_detect_1011_buggy.py
@@ -25,15 +25,15 @@ async def test_seq_bug1(dut):
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
 
-    dut.inp_bit.value = 11
+    dut.inp_bit.value = 0
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 0
     await FallingEdge(dut.clk)
-    dut.inp_bit.value = 9
+    dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
-    dut.inp_bit.value = 3
+    dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
 
     assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 11011. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
